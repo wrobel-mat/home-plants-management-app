81d8cdb624f8e4d97e412762a08a8ffa3cee3393
wrobel_mat <wrobel_mat@icloud.com>
2022-02-09 10:40:47 +0100

    [frontend]: add about page

--- frontend/src/views/About.js
+++ frontend/src/views/About.js
@@ -1,14 +1,20 @@
 import { useLocalizedStrings } from 'providers/LocalizedStringsProvider'
 import MainSectionContainer from "components/containers/MainSectionContainer/MainSectionContainer";
 import PageHeader from 'components/atoms/PageHeader/PageHeader'
-import WebsiteUnderConstruction from 'components/atoms/WebsiteUnderConstruction/WebsiteUnderConstruction'
+import WelcomePageContentContainer from 'components/containers/WelcomePageContentContainer/WelcomePageContentContainer';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
 
 export default function About() {
   const { strings } = useLocalizedStrings();
   return (
     <MainSectionContainer>
       <PageHeader title={strings.aboutPage.title}/>
-      <WebsiteUnderConstruction/>
+      <WelcomePageContentContainer>
+        <Paragraph text={strings.welcomePage.firstParagraph} />
+        <Paragraph text={strings.welcomePage.secondParagraph} />
+        <Paragraph text={strings.welcomePage.thirdParagraph} />
+        <Paragraph text={strings.welcomePage.fourthParagraph} />
+      </WelcomePageContentContainer>
     </MainSectionContainer>
   );
 }
